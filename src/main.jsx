5f57671bcc530e2ec7f7dfd6e7f346e71b2b9d42
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-05 22:39:08 -0400

    init

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { FirstApp } from './FirstApp'
+import { HelloWorldApp } from './HelloWorldApp'
+import { CounterApp } from './CounterApp'
+
+import './style.css';
+
+
+
+ReactDOM.createRoot( document.getElementById('root') ).render( 
+    <React.StrictMode>
+        <HelloWorldApp />
+        <CounterApp  />
+        
+    </React.StrictMode>
+)
