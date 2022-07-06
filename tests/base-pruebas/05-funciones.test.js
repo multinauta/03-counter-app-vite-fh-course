15af652850c94bbfb28f605ae41ad2f5dbebb0a1
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-06 00:38:22 -0400

    class 55

---
+++ tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,29 @@
+import { getUser, getUsuarioActivo } from '../../src/base-pruebas/05-funciones';
+
+describe('Pruebas en 05-funciones' , () => {
+
+    test('getUser debe de retornar un objeto', () => {
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        };
+
+        const user = getUser();
+
+        expect( testUser ).toEqual( user );
+});
+
+    test('get UsuatioActivo debe retornar un objeto', () => {
+
+        const name = 'Alvaro';
+
+        const user = getUsuarioActivo( name );
+
+        expect( user ).toStrictEqual({
+            uid: 'ABC567',
+            username: name
+        })
+
+    });
+
+});
